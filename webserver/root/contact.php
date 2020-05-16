b74a00990f2c1e5219b196e7b366806ab13fea8a
tamaraMelcher <t.melcher.htlleonding@gmail.com>
2020-05-16 13:53:57 +0200

    Contact (wird noch verbessert)
    dein text!!

--- webserver/root/contact.php
+++ webserver/root/contact.php
@@ -44,20 +44,68 @@
              </div>
          </nav>
      <section>
-         <div class="container text-center">
-             <div class="card border-0 shadow my-5">
-                 <div class="card-body p-5">
-                     <div>
-                         <img class="img-thumbnail" src="Pictures\Anna.jpg" alt="Anna" >
-                         <div class="desc">Add a description of the image here</div>
+              <div class="container text-center">
+                  <div class="card border-0 shadow my-5">
+                  <div class="about-section">
+                    <h1>About Us Page</h1>
+                    <p>Some text about who we are and what we do.</p>
+                    <p>Resize the browser window to see that this page is responsive by the way.</p>
+                  </div>
 
-                     </div>
-                     <div>
-                         <img class="img-thumbnail" src="Pictures\Tamara.jpg" alt="Anna" >
-                         <div class="desc">Add a description of the image here</div>
-                     </div>
-                 </div>
-             </div>
-         </div>
+                  <h2 style="text-align:center">Our Team</h2>
+                  <div class="row">
+                    <div class="column">
+                      <div class="card">
+                        <img src="Pictures\Anna.jpg" alt="Anna" style="width:100%">
+                        <div class="container">
+                          <h2>Jane Doe</h2>
+                          <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                          <p>jane@example.com</p>
+                          <p><button class="button">Contact</button></p>
+                        </div>
+                      </div>
+                    </div>
+
+
+
+                    <div class="column">
+                      <div class="card">
+                        <img src="Pictures/Tamara.jpg" alt="Tamara" style="width:100%">
+                        <div class="container">
+                          <h2>Tamara Melcher</h2>
+                          <p>My name is Tamara Melcher. I am 16 years old and I visit the HTL Leonding for IT.</p>
+                          <p>One of my friends and I did this fashion website as a project for our school.</p>
+                          <p>We thought it would be a great idea to do this because we want to give women and men an insight into fashion.</p>
+                          <p>In our society, most people think that fashion is only something for women, therefore we want to change this biased attitude a bit.</p>
+                          <a href="https://www.instagram.com/tami.melcher/"><br /><br />tami.melcher</a>
+                          <a href="mailto:t.melcher.htlleonding@gmail.com?subject=Feedback&body=Message">
+                            <br />t.melcher.htlleonding@gmail.com
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+
+              </div>
      </section>
  </body>
+</div>
+                      <div class="card-body p-5">
+                          <div>
+                              <img class="img-thumbnail" src="Pictures\Anna.jpg" alt="Anna" >
+                              <div class="desc">Add a description of the image here</div>
+
+                          </div>
+                          <div>
+                              <img class="img-thumbnail" src="Pictures\Tamara.jpg" alt="Anna" >
+                              <div class="desc">My name is Tamara Melcher. I am 16 years old and I visit the HTL Leonding for IT.
+                                  <br /> One of my friends and I did this fashion website as a project for our school.
+                                  <br />We thought it would be a great idea to do this because we want to give women and men an insight into fashion.
+                                  <br />In our society, most people think that fashion is only something for women, therefore we want to change this biased attitude a bit.
+                                  <a href="https://www.instagram.com/tami.melcher/"><br /><br />tami.melcher</a>
+                                  <a href="mailto:t.melcher.htlleonding@gmail.com?subject=Feedback&body=Message">
+                                      <br />t.melcher.htlleonding@gmail.com
+                                  </a>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
