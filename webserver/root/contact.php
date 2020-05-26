30c1ca8b4b87ed8f58da780014c113dc4f880d80
tamaraMelcher <t.melcher.htlleonding@gmail.com>
2020-05-26 12:30:31 +0200

    new bottom

--- webserver/root/contact.php
+++ webserver/root/contact.php
@@ -89,13 +89,7 @@
                       </div>
 
                       <div class="bottom-margin">
-
                         <p><br>We did this fashion website as a project for our school. <br>
-
-                        <hr>
-                        <h2>Our Philosophy</h2>
-
-                        <p>We did this fashion website as a project for our school. <br>
                         We thought it would be a great idea to do this because we want to give women and men an insight into fashion. <br>
                         In our society, most people think that fashion is only something for women, therefore we want to change this biased attitude a bit.</p>
                          <hr>
