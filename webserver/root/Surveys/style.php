106538be515d74a7e139ebe31d797b689fc2638c
Anna Hartl <AnnaHartl@users.noreply.github.com>
2020-05-29 10:38:08 +0200

    edited radio buttons
    on survey page

--- webserver/root/Surveys/style.php
+++ webserver/root/Surveys/style.php
@@ -66,62 +66,62 @@ include "../likes.php";
                 <h3>It's summer time! <br>What swimwear would you wear so the beach?<br><br></h3>
                 <img class="survey-img survey-img-left" src="../Pictures/Survey/style/beach1.jpg">
                 <img class="survey-img" src="../Pictures/Survey/style/beach2.jpg"><br>
-                <input class="radio-btn-survey" type="radio" id="beach" name="beach" value="1" checked="checked">
-                <input class="radio-btn-survey" type="radio" id="beach" name="beach" value="2"><br>
+                <input class="radio-btn-survey checkmark" type="radio" id="beach" name="beach" value="1" checked="checked">
+                <input class="radio-btn-survey checkmark" type="radio" id="beach" name="beach" value="2"><br>
                 <img class="survey-img survey-img-left" src="../Pictures/Survey/style/beach3.jpg">
                 <img class="survey-img" src="../Pictures/Survey/style/beach4.jpg">
-                <input class="radio-btn-survey" type="radio" id="beach" name="beach" value="3">
-                <input class="radio-btn-survey" type="radio" id="beach" name="beach" value="4">
+                <input class="radio-btn-survey checkmark" type="radio" id="beach" name="beach" value="3">
+                <input class="radio-btn-survey checkmark" type="radio" id="beach" name="beach" value="4">
                 <br> <br> <hr> <br>
                 <h3>How would your friend describe you?<br><br></h3>
-                <input type="radio" id="personality" name="personality" value="1" checked="checked">
+                <input class="checkmark" type="radio" id="personality" name="personality" value="1" checked="checked">
                 <label for="1">Cool, laidback, having an attitude</label><br>
-                <input type="radio" id="personality" name="personality" value="2">
+                <input class="checkmark" type="radio" id="personality" name="personality" value="2">
                 <label for="2">Girly, playful, happy, likes attention</label><br>
-                <input type="radio" id="personality" name="personality" value="3">
+                <input class="checkmark" type="radio" id="personality" name="personality" value="3">
                 <label for="3">Mature, thinks about her actions, confident</label><br>
-                <input type="radio" id="personality" name="personality" value="4">
+                <input class="checkmark" type="radio" id="personality" name="personality" value="4">
                 <label for="4">Fun, energetic, extroverted</label><br>
                 <br> <br> <hr> <br>
                 <h3>Your bestfriend gets you a necklace for your birthday <br>How would it look? <br><br></h3>
                 <img class="survey-img survey-img-left" src="../Pictures/Survey/style/gift1.jpg">
                 <img class="survey-img" src="../Pictures/Survey/style/gift2.jpg"><br>
-                <input class="radio-btn-survey" type="radio" id="gift" name="gift" value="1" checked="checked">
-                <input class="radio-btn-survey" type="radio" id="gift" name="gift" value="2"><br>
+                <input class="radio-btn-survey checkmark" type="radio" id="gift" name="gift" value="1" checked="checked">
+                <input class="radio-btn-survey checkmark" type="radio" id="gift" name="gift" value="2"><br>
                 <img class="survey-img survey-img-left" src="../Pictures/Survey/style/gift3.jpg">
                 <img class="survey-img" src="../Pictures/Survey/style/gift4.jpg">
-                <input class="radio-btn-survey" type="radio" id="gift" name="gift" value="3">
-                <input class="radio-btn-survey" type="radio" id="gift" name="gift" value="4">
+                <input class="radio-btn-survey checkmark" type="radio" id="gift" name="gift" value="3">
+                <input class="radio-btn-survey checkmark" type="radio" id="gift" name="gift" value="4">
                 <br> <br> <hr> <br>
                 <h3>Your favourite shoes?<br><br></h3>
                 <img class="survey-img survey-img-left" src="../Pictures/Survey/style/shoes1.jpg">
                 <img class="survey-img" src="../Pictures/Survey/style/shoes2.jpg"><br>
-                <input class="radio-btn-survey" type="radio" id="shoes" name="shoes" value="1" checked="checked">
-                <input class="radio-btn-survey" type="radio" id="shoes" name="shoes" value="2"><br>
+                <input class="radio-btn-survey checkmark" type="radio" id="shoes" name="shoes" value="1" checked="checked">
+                <input class="radio-btn-survey checkmark" type="radio" id="shoes" name="shoes" value="2"><br>
                 <img class="survey-img survey-img-left" src="../Pictures/Survey/style/shoes3.jpg">
                 <img class="survey-img" src="../Pictures/Survey/style/shoes4.jpg">
-                <input class="radio-btn-survey" type="radio" id="shoes" name="shoes" value="3">
-                <input class="radio-btn-survey" type="radio" id="shoes" name="shoes" value="4">
+                <input class="radio-btn-survey checkmark" type="radio" id="shoes" name="shoes" value="3">
+                <input class="radio-btn-survey checkmark" type="radio" id="shoes" name="shoes" value="4">
                 <br> <br> <hr> <br>
                 <h3>You found your perfect match <br>What is your dream weddingdress?<br><br></h3>
                 <img class="survey-img survey-img-left" src="../Pictures/Survey/style/wedding1.jpg">
                 <img class="survey-img" src="../Pictures/Survey/style/wedding2.jpg"><br>
-                <input class="radio-btn-survey" type="radio" id="wedding" name="wedding" value="1" checked="checked">
-                <input class="radio-btn-survey" type="radio" id="wedding" name="wedding" value="2"><br>
+                <input class="radio-btn-survey checkmark" type="radio" id="wedding" name="wedding" value="1" checked="checked">
+                <input class="radio-btn-survey checkmark" type="radio" id="wedding" name="wedding" value="2"><br>
                 <img class="survey-img survey-img-left" src="../Pictures/Survey/style/wedding3.jpg">
                 <img class="survey-img" src="../Pictures/Survey/style/wedding4.jpg">
-                <input class="radio-btn-survey" type="radio" id="wedding" name="wedding" value="3">
-                <input class="radio-btn-survey" type="radio" id="wedding" name="wedding" value="4">
+                <input class="radio-btn-survey checkmark" type="radio" id="wedding" name="wedding" value="3">
+                <input class="radio-btn-survey checkmark" type="radio" id="wedding" name="wedding" value="4">
                 <br> <br> <hr> <br>
                 <h3>Party time! <br> What should you wear?<br><br></h3>
                 <img class="survey-img survey-img-left" src="../Pictures/Survey/style/clubbing1.jpg">
                 <img class="survey-img" src="../Pictures/Survey/style/clubbing2.jpg"><br>
-                <input class="radio-btn-survey" type="radio" id="club" name="club" value="1" checked="checked">
-                <input class="radio-btn-survey" type="radio" id="club" name="club" value="2"><br>
+                <input class="radio-btn-survey checkmark" type="radio" id="club" name="club" value="1" checked="checked">
+                <input class="radio-btn-survey checkmark" type="radio" id="club" name="club" value="2"><br>
                 <img class="survey-img survey-img-left" src="../Pictures/Survey/style/clubbing3.jpg">
                 <img class="survey-img" src="../Pictures/Survey/style/clubbing4.jpg">
-                <input class="radio-btn-survey" type="radio" id="club" name="club" value="3">
-                <input class="radio-btn-survey" type="radio" id="club" name="club" value="4">
+                <input class="radio-btn-survey checkmark" type="radio" id="club" name="club" value="3">
+                <input class="radio-btn-survey checkmark" type="radio" id="club" name="club" value="4">
                 <br> <br> <hr> <br>
 
                 <button id="submit" name="submit" class="btn btn-primary" style="padding-left: 12px;">Result</button>
